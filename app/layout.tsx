feb6ffcc020ced234f8cf303bb5f76693d0f2757
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-09-06 16:42:29 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,13 +36,14 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
+                  <div className="flex gap-5 items-center font-bold text-xl">
+                    <Link href={"/"}>SD Kudangan 2</Link>
+                    {/* <div className="flex items-center gap-2">
                       <DeployButton />
-                    </div>
+                    </div> */}
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <ThemeSwitcher />
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
@@ -61,7 +62,7 @@ export default function RootLayout({
                     Supabase
                   </a>
                 </p>
-                <ThemeSwitcher />
+                
               </footer>
             </div>
           </main>
--- components/hero.tsx
+++ components/hero.tsx
@@ -4,41 +4,13 @@ import SupabaseLogo from "./supabase-logo";
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
-      </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      
+      <h1 className="sr-only">SD Kudangan 2</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
+        
+        <span className="font-bold">SD Kudangan 2 </span> adalah sekolah yang mengedepankan siswa untuk mendapatkan perhatian maksimal perihal <span className="bg-green-200">intelektual</span> dan <span className="bg-red-300">kerohanian</span>
       </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+      {/* <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" /> */}
     </div>
   );
 }
